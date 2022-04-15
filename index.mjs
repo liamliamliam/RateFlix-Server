2a3d1311157e925bd93d4bdc632d37130cffa7df
Liam Bell <courseemaily@gmail.com>
2022-04-15 10:32:39 +1000

    ...

--- index.mjs
+++ index.mjs
@@ -24,8 +24,7 @@ app.use(bodyParser.json());
 app.use(
   cookieSession({
     maxAge: 30 * 24 * 60 * 60 * 1000,
-    keys: [process.env.COOKIE_KEY],
-    domain: 'vercel.com'
+    keys: [process.env.COOKIE_KEY]
   })
 );
 app.use(passport.initialize());
