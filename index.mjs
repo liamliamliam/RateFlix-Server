9b0d6c75f6d0c7ea3b8db83b761f5c388e5a22d3
Liam Bell <courseemaily@gmail.com>
2022-04-16 16:17:47 +1000

    ...

--- index.mjs
+++ index.mjs
@@ -24,9 +24,7 @@ app.use(bodyParser.json());
 app.use(
   cookieSession({
     maxAge: 30 * 24 * 60 * 60 * 1000,
-    keys: [process.env.COOKIE_KEY],
-    domain: 'rateflix.vercel.app',
-    secure: true
+    keys: [process.env.COOKIE_KEY]
   })
 );
 app.use(passport.initialize());
